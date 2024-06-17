cffd181416181897ee5dea5a1077ab8358a0c6d5
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2024-06-17 17:30:24 +0700

    first commit

---
+++ springSecurityL7/src/main/java/ru/istomin/springSecurityL7/SpringSecurityL7Application.java
@@ -0,0 +1,13 @@
+package ru.istomin.springSecurityL7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityL7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityL7Application.class, args);
+	}
+
+}
---
+++ springSecurityL7/src/test/java/ru/istomin/springSecurityL7/SpringSecurityL7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.istomin.springSecurityL7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityL7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
